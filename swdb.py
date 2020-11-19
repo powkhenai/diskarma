c516636a85f2982f971fd9f17af691972d69727f
Jake Allen <jakedallen@me.com>
2020-11-19 01:30:35 -0500

    Add images to the who_is command

--- swdb.py
+++ swdb.py
@@ -8,35 +8,27 @@ class swdb():
 
     def query_name(self, name):
         url = '{0}monsters'.format(self.__url_prefix)
-        #print(url)
         payload = {'name': name}
         response = requests.request('get', url, params=payload)
-        #print(response)
-        #print(response.text)
         if response.json()['results']:
             monster = response.json()['results'][0]
-            #print(monster['name'])
-            #print(monster['awakens_from'])
             return monster['awakens_from']
         else:
-            #print('I can\'t find that name...')
             return None
 
 
     def query_id(self, id):
         url = '{0}monsters/{1}/'.format(self.__url_prefix, id)
         response = requests.request('get', url)
-        #print(response)
-        #print(response.text)
         monster = response.json()
-        #print('{element} {name}'.format(element=monster['element'], name=monster['name']))
-        return '{element} {name}'.format(element=monster['element'], name=monster['name'])
+        return (monster['name'], monster['element'], monster['image_filename'])
 
     def who_is(self, name):
         type_id = self.query_name(name)
-        #print(type_id)
+        name, element, image = (self.query_id(type_id))
+        monster_info = {'title': '{0} {1}'.format(element, name), 'set_image': '{0}{1}'.format(self.__image_url, image)}
         if type_id:
-            return self.query_id(type_id)
+            return monster_info
         return 'I don\'t know what monster that is, maybe check your spelling?'
 
 if __name__ == '__main__':
